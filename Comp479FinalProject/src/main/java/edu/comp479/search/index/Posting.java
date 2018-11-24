0fa53c30c0aa0cbb1c0b2c1d115fd7a2d5f5c4e9
SomeMatthew <someMathew@outlook.com>
2018-11-24 02:47:22 -0500

    Modification to the folder structure to standard maven structure.
    The structure was initially incorrectly setup by Eclipse, it has been
    refactored to the standard structure with src/main, src/test and
    resources folder.

--- Comp479FinalProject/src/main/java/edu/comp479/search/index/Posting.java
+++ Comp479FinalProject/src/main/java/edu/comp479/search/index/Posting.java
@@ -2,17 +2,22 @@ package edu.comp479.search.index;
 
 public class Posting {
     private final long docId;
+    private final int termFreq;
     private final float tfIdf;
 
-    public Posting(long docId, float tfIdf) {
+    public Posting(long docId, int termFreq, float tfIdf) {
         if (docId < 0) {
             throw new IllegalArgumentException("docId must be a non-negative integer");
+        }
+        if (termFreq < 1) {
+            throw new IllegalArgumentException("TermFreq must be >= 1");
         }
         if (tfIdf < 0) {
             throw new IllegalArgumentException("The tf-idf should not be negative");
         }
         this.docId = docId;
         this.tfIdf = tfIdf;
+        this.termFreq = termFreq;
     }
 
     public long getDocId() {
@@ -21,6 +26,10 @@ public class Posting {
 
     public float getTfIdf() {
         return tfIdf;
+    }
+
+    public int getTermFreq() {
+        return termFreq;
     }
 
 }
