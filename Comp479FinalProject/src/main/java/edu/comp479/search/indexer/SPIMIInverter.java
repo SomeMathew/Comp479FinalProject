ea87c7754a80043e1537d4357f575a19b0c62a9d
SomeMatthew <someMathew@outlook.com>
2018-11-26 00:20:38 -0500

    SPIMInverter Test Suite and bug fix.
    The count of the blocks were not incrementing.

--- Comp479FinalProject/src/main/java/edu/comp479/search/indexer/SPIMIInverter.java
+++ Comp479FinalProject/src/main/java/edu/comp479/search/indexer/SPIMIInverter.java
@@ -3,6 +3,7 @@ package edu.comp479.search.indexer;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +11,7 @@ import edu.comp479.search.indexer.file.IndexWriter;
 import edu.comp479.search.indexer.file.IndexDataMapperFactory;
 import edu.comp479.search.tokenizer.IToken;
 import edu.comp479.search.tokenizer.ITokenStream;
+import edu.comp479.search.util.SentimentDictionaryBuilder;
 
 import static com.google.common.base.Preconditions.*;
 
@@ -17,7 +19,7 @@ public class SPIMIInverter {
     private static final Logger LOGGER = Logger.getLogger(SPIMIInverter.class.getName());
 
     public static final String DEFAULT_DIRECTORY = ".";
-    public static final int DEFAULT_MIN_MEMORY_USE = 32;
+    public static final int MIN_MEMORY_USE = 32;
     public static final int DEFAULT_MAX_MEMORY_USE = 64;
 
     private final String indexName;
@@ -27,17 +29,37 @@ public class SPIMIInverter {
 
     private final IndexBlockBuilderFactory blockBuilderFactory;
     private final IndexDataMapperFactory indexWriterFactory;
+    private final Runtime runtime;
 
     private int blockCount = 0;
 
     /**
-     * @see SPIMIInverter#SPIMIInverter(String, ITokenStream, long, Path,
-     *      IndexBlockBuilderFactory, IndexDataMapperFactory)
+     * @throws IOException If the sentiment dictionary cannot be loaded.
+     * @see SPIMIInverter#SPIMIInverter(String, ITokenStream, Path, long,
+     *      IndexBlockBuilderFactory, IndexDataMapperFactory, Runtime)
      */
-    public SPIMIInverter(String indexName, ITokenStream tokenStream, IndexBlockBuilderFactory blockBuilderFactory,
-            IndexDataMapperFactory indexDataMapperFactory) {
-        this(indexName, tokenStream, DEFAULT_MAX_MEMORY_USE, Paths.get(DEFAULT_DIRECTORY), blockBuilderFactory,
-                indexDataMapperFactory);
+    public SPIMIInverter(String indexName, ITokenStream tokenStream) throws IOException {
+        this(indexName, tokenStream, Paths.get(DEFAULT_DIRECTORY));
+    }
+
+    /**
+     * @throws IOException If the sentiment dictionary cannot be loaded.
+     * @see SPIMIInverter#SPIMIInverter(String, ITokenStream, Path, long,
+     *      IndexBlockBuilderFactory, IndexDataMapperFactory, Runtime)
+     */
+    public SPIMIInverter(String indexName, ITokenStream tokenStream, Path directory) throws IOException {
+        this(indexName, tokenStream, directory, DEFAULT_MAX_MEMORY_USE,
+                new SentimentDictionaryBuilder().loadSentimentDictionary());
+    }
+
+    /**
+     * @see SPIMIInverter#SPIMIInverter(String, ITokenStream, Path, long,
+     *      IndexBlockBuilderFactory, IndexDataMapperFactory, Runtime)
+     */
+    public SPIMIInverter(String indexName, ITokenStream tokenStream, Path directory, long maxMemoryUsageMb,
+            Map<String, Integer> sentimentDictionary) {
+        this(indexName, tokenStream, directory, maxMemoryUsageMb, new IndexBlockBuilderFactory(sentimentDictionary),
+                new IndexDataMapperFactory(), Runtime.getRuntime());
     }
 
     /**
@@ -46,25 +68,28 @@ public class SPIMIInverter {
      * @param indexName              Prefix name for this index, each block will be
      *                               individually parameterized.
      * @param tokenStream            Stream of {@link ITokenStream} to process.
+     * @param directory              Directory to write the blocks to.
      * @param maxMemoryUsageMb       Maximum program memory usage before a new block
      *                               is created. Must be at least
-     *                               {@value #DEFAULT_MIN_MEMORY_USE}.
-     * @param directory              Directory to write the blocks to.
+     *                               {@value #MIN_MEMORY_USE}.
      * @param blockBuilderFactory    Factory of Block Builder.
      * @param indexDataMapperFactory Factory of Datamapper to write the blocks to
      *                               disk.
+     * @param runtime                The java application runtime object.
      */
-    public SPIMIInverter(String indexName, ITokenStream tokenStream, long maxMemoryUsageMb, Path directory,
-            IndexBlockBuilderFactory blockBuilderFactory, IndexDataMapperFactory indexDataMapperFactory) {
+    public SPIMIInverter(String indexName, ITokenStream tokenStream, Path directory, long maxMemoryUsageMb,
+            IndexBlockBuilderFactory blockBuilderFactory, IndexDataMapperFactory indexDataMapperFactory,
+            Runtime runtime) {
         this.indexName = checkNotNull(indexName);
         checkArgument(!indexName.isEmpty(), "Index Name cannot be empty.");
         this.tokenStream = checkNotNull(tokenStream);
         this.directory = checkNotNull(directory);
         this.blockBuilderFactory = checkNotNull(blockBuilderFactory);
         this.indexWriterFactory = checkNotNull(indexDataMapperFactory);
+        this.runtime = checkNotNull(runtime);
 
-        if (maxMemoryUsageMb < DEFAULT_MIN_MEMORY_USE) {
-            maxMemoryUsageMb = DEFAULT_MIN_MEMORY_USE;
+        if (maxMemoryUsageMb < MIN_MEMORY_USE) {
+            maxMemoryUsageMb = MIN_MEMORY_USE;
         }
         this.maxMemoryUsageMb = maxMemoryUsageMb;
     }
@@ -93,6 +118,7 @@ public class SPIMIInverter {
         if (builder.getSize() > 0) {
             try (IndexWriter indexWriter = indexWriterFactory.createIndexWriter(indexBlockName, directory)) {
                 builder.writeToDisk(indexWriter);
+                blockCount++;
             } catch (IOException e) {
                 LOGGER.log(Level.SEVERE, "Unable to write the index Block.", e);
                 return null;
@@ -109,7 +135,6 @@ public class SPIMIInverter {
      * @return {@code true} if the limit is hit, {@code false} otherwise
      */
     private boolean validateMemoryUsage() {
-        Runtime runtime = Runtime.getRuntime();
         long totalMemoryUsage = runtime.totalMemory();
         return totalMemoryUsage < this.maxMemoryUsageMb;
     }
---
+++ Comp479FinalProject/src/test/java/edu/comp479/search/indexer/SPIMIInverterTest.java
@@ -0,0 +1,209 @@
+package edu.comp479.search.indexer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import edu.comp479.search.indexer.file.IndexDataMapperFactory;
+import edu.comp479.search.indexer.file.IndexWriter;
+import edu.comp479.search.tokenizer.IToken;
+import edu.comp479.search.tokenizer.ITokenStream;
+
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+@ExtendWith(MockitoExtension.class)
+class SPIMIInverterTest {
+
+    SPIMIInverter spimiInverter;
+
+    @Mock
+    private ITokenStream tokenStreamMock;
+
+    @Mock
+    private IndexBlockBuilderFactory blockBuilderFactoryMock;
+
+    @Mock
+    private IndexDataMapperFactory indexDataMapperFactoryMock;
+
+    @Mock
+    private IndexWriter indexWriterMock;
+
+    @Mock
+    private IndexBlockBuilder indexBlockBuilderMock;
+
+    @Mock
+    private Runtime runtime;
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        spimiInverter = new SPIMIInverter("test", tokenStreamMock, Paths.get("./TestIndex/"), 128l,
+                blockBuilderFactoryMock, indexDataMapperFactoryMock, runtime);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    void testPostingsAreAddedToTheBlockBuilder() {
+        when(blockBuilderFactoryMock.createIndexBlockBuilder()).thenReturn(indexBlockBuilderMock);
+        when(runtime.totalMemory()).thenReturn(1l);
+
+        when(tokenStreamMock.hasNext()).then(returnTrueTimes(3));
+        when(tokenStreamMock.next()).thenReturn(new Token("term1", 0), new Token("term2", 1), new Token("term3", 2));
+
+        spimiInverter.invert();
+
+        verify(indexBlockBuilderMock, times(1)).addPosting("term1", 0l);
+        verify(indexBlockBuilderMock, times(1)).addPosting("term2", 1l);
+        verify(indexBlockBuilderMock, times(1)).addPosting("term3", 2l);
+        verify(indexBlockBuilderMock, times(3)).addPosting(anyString(), anyLong());
+    }
+
+    @Test
+    void testBuildIndexCalledWhenComplete() throws IOException {
+        when(blockBuilderFactoryMock.createIndexBlockBuilder()).thenReturn(indexBlockBuilderMock);
+        when(indexDataMapperFactoryMock.createIndexWriter(any(), any())).thenReturn(indexWriterMock);
+
+        when(runtime.totalMemory()).thenReturn(1l);
+
+        when(tokenStreamMock.hasNext()).then(returnTrueTimes(3));
+        when(tokenStreamMock.next()).thenReturn(new Token("term1", 0), new Token("term2", 1), new Token("term3", 2));
+
+        when(indexBlockBuilderMock.getSize()).thenReturn(3);
+
+        spimiInverter.invert();
+
+        verify(indexBlockBuilderMock, times(1)).writeToDisk(eq(indexWriterMock));
+    }
+
+    @Test
+    void testReturnANewblockWhenMemoryLimitIsHit() throws IOException {
+        when(blockBuilderFactoryMock.createIndexBlockBuilder()).thenReturn(indexBlockBuilderMock);
+        when(indexDataMapperFactoryMock.createIndexWriter(any(), any())).thenReturn(indexWriterMock);
+
+        when(runtime.totalMemory()).thenReturn(1l, 1l, 128l);
+
+        when(tokenStreamMock.hasNext()).then(returnTrueTimes(3));
+        when(tokenStreamMock.next()).thenReturn(new Token("term1", 0), new Token("term2", 1), new Token("term3", 2));
+
+        when(indexBlockBuilderMock.getSize()).thenReturn(2).thenReturn(1);
+
+        String blockName = spimiInverter.invert();
+
+        verify(blockBuilderFactoryMock, times(1)).createIndexBlockBuilder();
+        verify(tokenStreamMock, times(2)).next();
+        assertEquals("test_0.blk", blockName);
+    }
+
+    @Test
+    void testCreateANewblockWhenStreamIsFinished() throws IOException {
+        when(blockBuilderFactoryMock.createIndexBlockBuilder()).thenReturn(indexBlockBuilderMock);
+        when(indexDataMapperFactoryMock.createIndexWriter(any(), any())).thenReturn(indexWriterMock);
+
+        when(runtime.totalMemory()).thenReturn(1l, 128l, 1l);
+
+        when(tokenStreamMock.hasNext()).then(returnTrueTimes(3));
+        when(tokenStreamMock.next()).thenReturn(new Token("term1", 0), new Token("term2", 1), new Token("term3", 2));
+        when(indexBlockBuilderMock.getSize()).thenReturn(3);
+
+        String block1Name = spimiInverter.invert();
+        String block2Name = spimiInverter.invert();
+
+        verify(blockBuilderFactoryMock, times(2)).createIndexBlockBuilder();
+        verify(indexDataMapperFactoryMock, times(2)).createIndexWriter(any(), any());
+        verify(indexBlockBuilderMock, times(2)).writeToDisk(eq(indexWriterMock));
+        assertTrue(!block1Name.equals(block2Name), "Block1: " + block1Name + ", Block2: " + block2Name);
+    }
+
+    @Test
+    void testCreateNewBlockWhenRestartedAfterMemLimitHit() throws IOException {
+        when(blockBuilderFactoryMock.createIndexBlockBuilder()).thenReturn(indexBlockBuilderMock);
+        when(indexDataMapperFactoryMock.createIndexWriter(any(), any())).thenReturn(indexWriterMock);
+
+        when(runtime.totalMemory()).thenReturn(1l);
+        when(tokenStreamMock.hasNext()).then(returnTrueTimes(3));
+
+        when(tokenStreamMock.next()).thenReturn(new Token("term1", 0), new Token("term2", 1), new Token("term3", 2));
+        when(indexBlockBuilderMock.getSize()).thenReturn(3);
+
+        spimiInverter.invert();
+
+        verify(blockBuilderFactoryMock, times(1)).createIndexBlockBuilder();
+        verify(indexDataMapperFactoryMock, times(1)).createIndexWriter(any(), any());
+        verify(indexBlockBuilderMock, times(1)).writeToDisk(eq(indexWriterMock));
+        verify(tokenStreamMock, times(3)).next();
+    }
+
+    @Test
+    void testReturnsNullOnIOException() throws IOException {
+        when(blockBuilderFactoryMock.createIndexBlockBuilder()).thenReturn(indexBlockBuilderMock);
+        when(indexDataMapperFactoryMock.createIndexWriter(any(), any())).thenThrow(IOException.class);
+
+        when(runtime.totalMemory()).thenReturn(1l);
+        when(tokenStreamMock.hasNext()).then(returnTrueTimes(3));
+        when(tokenStreamMock.next()).thenReturn(new Token("term1", 0), new Token("term2", 1), new Token("term3", 2));
+        when(indexBlockBuilderMock.getSize()).thenReturn(3);
+
+        String blockName = spimiInverter.invert();
+
+        assertNull(blockName, "Given: " + blockName);
+    }
+
+    private Answer<Boolean> returnTrueTimes(int times) {
+        return new Answer<Boolean>() {
+            int hasNextCalledTime = 0;
+
+            @Override
+            public Boolean answer(InvocationOnMock invocation) throws Throwable {
+                return hasNextCalledTime++ < times;
+            }
+        };
+    }
+
+    private static class Token implements IToken {
+        private String term;
+        private long docId;
+
+        public Token(String term, long docId) {
+            this.term = term;
+            this.docId = docId;
+        }
+
+        @Override
+        public String getTerm() {
+            return term;
+        }
+
+        @Override
+        public long getDocId() {
+            return docId;
+        }
+
+    }
+
+}
