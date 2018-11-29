8e1f26be28a55f85b6c52fb5be538fd2e4e9c40d
SomeMatthew <SomeMathew@outlook.com>
2018-11-29 13:22:52 -0500

    Addition of method to get the number of document in the index.

--- Comp479FinalProject/src/main/java/edu/comp479/search/index/IInvertedIndex.java
+++ Comp479FinalProject/src/main/java/edu/comp479/search/index/IInvertedIndex.java
@@ -3,23 +3,30 @@ package edu.comp479.search.index;
 import edu.comp479.search.index.structure.IIndexEntry;
 
 public interface IInvertedIndex {
-    /**
-     * Retrieves the details about a specific term from the inverted index.
-     * 
-     * If a term is not found in the index, it will return an {@link IIndexEntry}
-     * which has an empty Postings list and a {@code docFreq} of 0.
-     * 
-     * @param term Search Term
-     * @return Entry details and postings for the term.
-     */
-    public IIndexEntry getPostings(String term);
+	/**
+	 * Retrieves the details about a specific term from the inverted index.
+	 * 
+	 * If a term is not found in the index, it will return an {@link IIndexEntry}
+	 * which has an empty Postings list and a {@code docFreq} of 0.
+	 * 
+	 * @param term Search Term
+	 * @return Entry details and postings for the term.
+	 */
+	public IIndexEntry getPostings(String term);
 
-    /**
-     * Retrieves a document's length norm for normalizing the tf-idf cosine
-     * similarity score.
-     * 
-     * @param docId Document to retrieve.
-     * @return
-     */
-    public float getDocumentLengthNorm(long docId);
+	/**
+	 * Retrieves a document's length norm for normalizing the tf-idf cosine
+	 * similarity score.
+	 * 
+	 * @param docId Document to retrieve.
+	 * @return
+	 */
+	public float getDocumentLengthNorm(long docId);
+
+	/**
+	 * Get the number of documents in the inverted index.
+	 * 
+	 * @return the number of documents.
+	 */
+	public long getDocumentCount();
 }
--- Comp479FinalProject/src/main/java/edu/comp479/search/index/InvertedIndex.java
+++ Comp479FinalProject/src/main/java/edu/comp479/search/index/InvertedIndex.java
@@ -16,39 +16,44 @@ import edu.comp479.search.indexer.file.IndexReaderMemoryMapped;
 import edu.comp479.search.indexer.file.NormFileEntry;
 
 public class InvertedIndex implements IInvertedIndex {
-    private final IndexReaderMemoryMapped indexReader;
-    private final ImmutableMap<String, DictionaryEntry> dictionary;
-    private final Map<String, Integer> sentimentDictionary;
+	private final IndexReaderMemoryMapped indexReader;
+	private final ImmutableMap<String, DictionaryEntry> dictionary;
+	private final Map<String, Integer> sentimentDictionary;
 
-    public InvertedIndex(ImmutableMap<String, DictionaryEntry> dictionary, IndexReaderMemoryMapped indexReader,
-            Map<String, Integer> sentimentDictionary) {
-        this.indexReader = checkNotNull(indexReader);
-        this.sentimentDictionary = checkNotNull(sentimentDictionary);
-        this.dictionary = checkNotNull(dictionary);
-    }
+	public InvertedIndex(ImmutableMap<String, DictionaryEntry> dictionary, IndexReaderMemoryMapped indexReader,
+			Map<String, Integer> sentimentDictionary) {
+		this.indexReader = checkNotNull(indexReader);
+		this.sentimentDictionary = checkNotNull(sentimentDictionary);
+		this.dictionary = checkNotNull(dictionary);
+	}
 
-    @Override
-    public IIndexEntry getPostings(String term) {
-        checkNotNull(term);
-        checkArgument(!term.isEmpty(), "The term must not be empty.");
+	@Override
+	public IIndexEntry getPostings(String term) {
+		checkNotNull(term);
+		checkArgument(!term.isEmpty(), "The term must not be empty.");
 
-        DictionaryEntry dictEntry = dictionary.get(term);
-        if (dictEntry == null) {
-            return new IndexEntry(new DictionaryEntry(term, 0, sentimentDictionary.getOrDefault(term, 0)),
-                    ImmutableList.of());
-        } else {
-            ImmutableList<Posting> postings = indexReader.readPostings(dictEntry);
-            return new IndexEntry(dictEntry, postings);
-        }
-    }
+		DictionaryEntry dictEntry = dictionary.get(term);
+		if (dictEntry == null) {
+			return new IndexEntry(new DictionaryEntry(term, 0, sentimentDictionary.getOrDefault(term, 0)),
+					ImmutableList.of());
+		} else {
+			ImmutableList<Posting> postings = indexReader.readPostings(dictEntry);
+			return new IndexEntry(dictEntry, postings);
+		}
+	}
 
-    @Override
-    public float getDocumentLengthNorm(long docId) {
-        NormFileEntry normEntry = indexReader.readNormEntry(docId);
-        Verify.verify(normEntry.getDocId() == docId,
-                "The returned norm length by the index is invalid. Expected: %s, Got: %s", docId, normEntry.getDocId());
+	@Override
+	public float getDocumentLengthNorm(long docId) {
+		NormFileEntry normEntry = indexReader.readNormEntry(docId);
+		Verify.verify(normEntry.getDocId() == docId,
+				"The returned norm length by the index is invalid. Expected: %s, Got: %s", docId, normEntry.getDocId());
 
-        return normEntry.getNorm();
-    }
+		return normEntry.getNorm();
+	}
+
+	@Override
+	public long getDocumentCount() {
+		return indexReader.getDocCount();
+	}
 
 }
