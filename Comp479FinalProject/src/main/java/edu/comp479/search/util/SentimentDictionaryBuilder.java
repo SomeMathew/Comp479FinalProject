53593b2cc8f102ebe2d4035d69281f3c75e4ac8c
SomeMatthew <someMathew@outlook.com>
2018-11-29 21:28:53 -0500

    Update to the ClassLoader for DictionaryBuilder on Windows.

--- Comp479FinalProject/src/main/java/edu/comp479/search/util/SentimentDictionaryBuilder.java
+++ Comp479FinalProject/src/main/java/edu/comp479/search/util/SentimentDictionaryBuilder.java
@@ -3,20 +3,32 @@ package edu.comp479.search.util;
 import static java.util.stream.Collectors.toMap;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class SentimentDictionaryBuilder {
+    private static Logger LOGGER = Logger.getLogger(SentimentDictionaryBuilder.class.getName());
+
     private static final String SENTIMENT_DICTIONARY_RESOURCE = "AFINN/AFINN-111.txt";
 
     public SentimentDictionaryBuilder() {
     }
 
     public Map<String, Integer> loadSentimentDictionary() throws IOException {
-        ClassLoader cl = getClass().getClassLoader();
-        Path path = Paths.get(cl.getResource(SENTIMENT_DICTIONARY_RESOURCE).getFile());
+        URI uri = null;
+        try {
+            uri = ClassLoader.getSystemResource(SENTIMENT_DICTIONARY_RESOURCE).toURI();
+        } catch (URISyntaxException e) {
+            LOGGER.log(Level.WARNING, "Problem loading the AFINN dictionary", e);
+            return null;
+        }
+        Path path = Paths.get(uri);
 
         return Files.lines(path).map((line) -> line.split("\\t"))
                 .collect(toMap(entry -> entry[0], entry -> Integer.parseInt(entry[1])));
