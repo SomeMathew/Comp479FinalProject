38f970a84ff234e7dd5f1b625a5032c5715cf3e5
SomeMatthew <someMathew@outlook.com>
2018-11-29 20:10:11 -0500

    Documentation for the TokenizerNormalize

--- Comp479FinalProject/src/main/java/edu/comp479/search/tokenizer/TokenizerNormalize.java
+++ Comp479FinalProject/src/main/java/edu/comp479/search/tokenizer/TokenizerNormalize.java
@@ -17,15 +17,43 @@ public class TokenizerNormalize {
     private static Logger LOGGER = Logger.getLogger(TokenizerNormalize.class.getName());
     Analyzer analyzer;
 
+    /**
+     * Creates a new Tokenizer Normalizer which does a minimal amount of processing.
+     * 
+     * <p>
+     * Filters: <br>
+     * <ul>
+     * <li>Lower Case Filter
+     * <li>StopFilter
+     * <li>Lucene Classic Filter
+     * </ul>
+     * 
+     * <p>
+     * This Tokenizer uses the implementation of the Classic (Standard) Tokenizer of
+     * Apache Lucene.
+     * 
+     */
     public TokenizerNormalize() {
         analyzer = new ClassicAnalyzer();
     }
 
+    /**
+     * Tokenizes and normalizes the given text into a list of String tokens.
+     * 
+     * @param text Text to normalize and tokenize
+     * @return Lists of token for the given text.
+     */
     public List<String> analyze(String text) {
         List<String> tokens = new ArrayList<>();
         return analyzeAppendToList(text, tokens);
     }
 
+    /**
+     * Tokenizes and normalizes the given text into the given {@code list}.
+     * 
+     * @param text Text to normalize and tokenize
+     * @return The {@code list} given as argument.
+     */
     public List<String> analyzeAppendToList(String text, List<String> list) {
         checkNotNull(text);
         checkNotNull(list);
