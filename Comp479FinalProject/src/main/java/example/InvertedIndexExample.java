e611249d5cb5eb7552161af988b9cef3d9434e82
SomeMatthew <someMathew@outlook.com>
2018-11-29 20:02:04 -0500

    Example on how to interact with the Inverted Index.

---
+++ Comp479FinalProject/src/main/java/example/InvertedIndexExample.java
@@ -0,0 +1,30 @@
+package example;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import edu.comp479.search.index.IInvertedIndex;
+import edu.comp479.search.index.IndexFactory;
+import edu.comp479.search.index.structure.IIndexEntry;
+import edu.comp479.search.indexer.file.IndexReaderMemoryMapped;
+
+public class InvertedIndexExample {
+
+    public InvertedIndexExample() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String args[]) throws IOException {
+        IndexReaderMemoryMapped indexReader = new IndexReaderMemoryMapped("testIndex", Paths.get("./testIndex/index/"));
+        indexReader.open();
+        IndexFactory indexFactory = new IndexFactory();
+        IInvertedIndex index = indexFactory.getIndex(indexReader);
+
+        IIndexEntry indexEntry = index.getPostings("concordia");
+
+        System.out.println(indexEntry);
+
+        indexReader.close();
+    }
+
+}
