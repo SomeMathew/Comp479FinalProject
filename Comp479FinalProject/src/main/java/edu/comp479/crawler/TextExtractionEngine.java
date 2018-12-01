e2fa7360b6c41ef5d757f1cffaf1fd79c970a013
Marco Dagostino <dagostino.marco@outlook.com>
2018-11-30 23:22:15 -0500

    Fixed IO Exception

--- Comp479FinalProject/src/main/java/edu/comp479/crawler/TextExtractionEngine.java
+++ Comp479FinalProject/src/main/java/edu/comp479/crawler/TextExtractionEngine.java
@@ -5,7 +5,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;g
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
